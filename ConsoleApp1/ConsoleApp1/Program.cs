e581df510366c6bf403aebf9221809887636ce97
TranHuyHoang191103 <emcuabanhratu@gmail.com>
2024-05-10 09:07:12 +0700

    update genetic algorithm

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,57 +10,102 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Room r1 = new Room("123", "402", 35, true);
-            Room r2 = new Room("234", "502", 40, false);
-            Course c1=new Course("111","thuc tap lt",true,3);
-            Course c2=new Course("222","huong doi tuong",false,4);
-            List<Course> lst_c=new List<Course>();
-            lst_c.Add(c1);
-            Teacher teacher = new Teacher("123","Hoang",lst_c);
-            lst_c=new List<Course>();
-            lst_c.Add(c1);
-            lst_c.Add(c2);
-            Teacher t2 = new Teacher("234", "Nam", lst_c);
-            List<Room> lst_rooms = new List<Room>();
-            lst_rooms.Add(r1);
-            lst_rooms.Add(r2);
-            List<Teacher> lst_Teacher=new List<Teacher>();
-            lst_Teacher.Add(teacher);
-            lst_Teacher.Add(t2);
-            List<StudentGroup> lst_s=new List<StudentGroup>();
-            lst_s.Add(new StudentGroup("hs1", "111", "dhti12", 23));
-            lst_s.Add(new StudentGroup("hs2", "222", "dhti12", 32));
-            lst_s.Add(new StudentGroup("hs3", "111", "dhti13", 22));
+            var rooms = new List<Room>
+        {
+            new Room("123", "402", 35, true),
+            new Room("234", "502", 40, false),
+            new Room("235", "503", 40, false),
+            new Room("236", "504", 40, false),
+            new Room("124", "403", 40, true),
+            new Room("125", "404", 40, true),
 
-            Data data = new Data(lst_rooms,lst_Teacher,lst_c,lst_s);
+            // Add more rooms if needed
+        };
+
+            var courses = new List<Course>
+        {
+            new Course("111", "thuc tap lt", true, 3),
+            new Course("222", "huong doi tuong", false, 4),
+            new Course("112", "thuc tap huong doi tuong", true, 4),
+            new Course("223", "co so du lieu", false, 4),
+            new Course("225", "co so du lieu phan tan", false, 4),
+            new Course("226", "cong nghe phan mem", false, 2),
+            // Add more courses if needed
+        };
+
+            var teachers = new List<Teacher>
+        {
+            new Teacher("123", "Hoang", new List<Course> { courses[0], courses[3] }),
+            new Teacher("125", "Hoang", new List<Course> { courses[0], courses[3], courses[2] }),
+            new Teacher("234", "Nam", new List<Course> { courses[0], courses[1], courses[4], courses[5] }),
+            new Teacher("235", "Nam Uq", new List<Course> { courses[0], courses[1], courses[2], courses[3] }),
+            // Add more teachers if needed
+        };
+
+            var studentGroups = new List<StudentGroup>
+        {
+            new StudentGroup("hs1", "111", "dhti12", 23),
+            new StudentGroup("hs2", "222", "dhti12", 32),
+            new StudentGroup("hs3", "223", "dhti12", 32),
+            new StudentGroup("hs4", "112", "dhti12", 32),
+            new StudentGroup("hs5", "223", "dhti32", 32),
+            new StudentGroup("hs6", "223", "dhti32", 32),
+            new StudentGroup("hs7", "225", "dhti32", 32),
+            new StudentGroup("hs8", "111", "dhti32", 32),
+            new StudentGroup("hs9", "223", "dhti32", 32),
+            new StudentGroup("hs10", "112", "dhti32", 32),
+            new StudentGroup("hs11", "111", "dhti32", 32),
+            new StudentGroup("hs12", "226", "dhti32", 32),
+            new StudentGroup("hs13", "222", "dhti32", 32),
+            new StudentGroup("hs14", "223", "dhti32", 32),
+            new StudentGroup("hs15", "111", "dhti32", 32),
+            new StudentGroup("hs16", "112", "dhti32", 32),
             
-            List<Schedule> population= new List<Schedule>();
-            for(int i = 0; i < 100; i++)
+            // Add more student groups if needed
+        };
+
+            var data = new Data(rooms, teachers, courses, studentGroups);
+
+            // Generate initial population
+            var population = new List<Schedule>();
+            for (int i = 0; i < 100; i++)
             {
-                Schedule T = new Schedule(data);
-                T.Initialize();
-                population.Add(T);
+                var schedule = new Schedule(data);
+                schedule.Initialize();
+                population.Add(schedule);
             }
-            
+
+            // Genetic algorithm loop
             while (true)
             {
                 population.Sort();
-                if(population[0].getFitness()>=1) break;
-                List<Schedule> newGen =new List<Schedule>();
-                for(int i=0;i<10;i++)
-                    newGen.Add(population[i]);
-                Random rd = new Random(Guid.NewGuid().GetHashCode());
+                
+
+                if (population[0].getFitness() >= 1)
+                {
+                    population[0].display();
+
+                    break;
+                }
+                    
+
+                var newGeneration = new List<Schedule>();
+                for (int i = 0; i < 10; i++)
+                {
+                    newGeneration.Add(population[i]);
+                }
+
+                var random = new Random();
                 for (int i = 0; i < 90; i++)
                 {
-                    int len = population.Count;
-                    int r = rd.Next(0, len);
-                    Schedule tmp_1 = population[r];
-                    r = rd.Next(0, len);
-                    Schedule tmp_2 = population[r];
-                    Schedule tmp_3 = tmp_1.mate(tmp_2);
-                    newGen.Add(tmp_3);
+                    int index1 = random.Next(population.Count);
+                    int index2 = random.Next(population.Count);
+                    
+                    var offspring = population[index1].mate(population[index2]);
+                    newGeneration.Add(offspring);
                 }
-                population = newGen;
+
+                population = newGeneration;
             }
         }
     }
