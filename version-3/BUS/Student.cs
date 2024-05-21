5e6d08b96a5acaf71651bde974579a341192610a
TranHuyHoang191103 <95302951+TranHuyHoang191103@users.noreply.github.com>
2024-05-21 13:28:45 +0700

    Update Student.cs

--- version-3/BUS/Student.cs
+++ version-3/BUS/Student.cs
@@ -7,26 +7,31 @@ using System.Threading.Tasks;
 using DTO;
 namespace BUS
 {
-    public class Student
+   public Student_DTO add(Student_DTO t,int namHoc,string maNganh)
+{
+    string str = DataAccess.getObj("SELECT MAX(SinhVien.maSV)\r\nFROM SinhVien\r\nJOIN LopHoc ON SinhVien.MaLop = LopHoc.MaLop\r\nWHERE LopHoc.MaNganh = '" + maNganh + "t'");
+    string cv = namHoc.ToString();
+    string tmp = "";
+    if (!(str[0] == cv[cv.Length - 2] || (str[1] != cv[cv.Length - 1]) && str.Length != 0)|| str.Length==0)
     {
-        public string getNewID(Student_DTO t)
-        {
-            string str = DataAccess.getObj("SELECT MAX(SinhVien.maSV)\r\nFROM SinhVien\r\nJOIN LopHoc ON SinhVien.MaLop = LopHoc.MaLop\r\nWHERE LopHoc.MaNganh = '" + t.getNganh() + "t'");
-            string cv = t.getNamHoc().ToString();
-            int t1 = Convert.ToInt32(str);
-            t1++;
-            string tmp = t1.ToString();
-            if (!(str[0] == cv[cv.Length - 2] || (str[1] != cv[cv.Length - 1]) && str.Length != 0) || str.Length == 0)
-            {
-                tmp = cv + t.getNganh() + "00001";
-            }
-            return tmp;
-        }
-        public string getLopHoc(string maNganh,int namHoc)
-        {
-            string str = DataAccess.getObj("SELECT MaLop \r\nFROM LopHoc \r\nWHERE SoSinhVien < 50 \r\nAND SoSinhVien = (SELECT MAX(SoSinhVien) FROM LopHoc WHERE SoSinhVien < 50 AND MaNganh='"+maNganh+"'AND NamVaoTruong='"+namHoc+"');\r\n");
-            return str;
-        }
+        tmp = cv + maNganh + "00001";
+    }
+    else
+    {
+        int t1 = Convert.ToInt32(str);
+        t1++;
+        tmp = t1.ToString();
+    }
+    string maLop = getLopHoc(maNganh, namHoc);
+    t.setMaLop(maLop);
+    t.setID(tmp);
+    return t;
+}
+public string getLopHoc(string maNganh,int namHoc)
+{
+    string str = DataAccess.getObj("SELECT MaLop \r\nFROM LopHoc \r\nWHERE SoSinhVien < 50 \r\nAND SoSinhVien = (SELECT MAX(SoSinhVien) FROM LopHoc WHERE SoSinhVien < 50 AND MaNganh='"+maNganh+"'AND NamVaoTruong='"+namHoc+"');\r\n");
+    return str;
+}
         
     }
 }
